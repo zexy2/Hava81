3ff7458205330e4afda7df1758322d4244a4be99
zexy2 <zexy2@users.noreply.github.com>
2026-09-04 02:57:15 +0000

    deploy: 28cb2d4ec440853883e58216bc79d202555ce1e2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hava81-shell-0753459a8af9';
+const CACHE_NAME = 'hava81-shell-f503921e587c';
 const LEGACY_RELOAD_CACHE_NAMES = new Set(['hava81-shell-v1', 'hava81-shell-v2']);
 const OPTIONAL_SHELL = ['/manifest.json'];
 
